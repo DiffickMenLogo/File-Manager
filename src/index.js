a8861e1ef0b567364838d0e87bf02d0990f158d7
DiffickMenLogo <gh.sdfs@mail.ru>
2022-12-19 16:16:34 +0300

    feat: update index.js to all modules in program

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import { dirname } from "path";
 import up from "./nwd/up.js";
 import cd from "./nwd/cd.js";
 import ls from "./nwd/ls.js";
+import cat from "./fo/cat.js";
+import add from "./fo/add.js";
+import rn from "./fo/rn.js";
+import mv from "./fo/mv.js";
+import cp from "./fo/cp.js";
+import rm from "./fo/rm.js";
+import os from "./os/os.js";
+import hash from "./hash/hash.js";
+import compress from "./archivate/compress.js";
+import decompress from "./archivate/decompress.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 let currentDir = __dirname;
@@ -45,6 +55,41 @@ const menu = function () {
       }
     } else if (answer.startsWith("ls")) {
       ls(currentDir);
+    } else if (answer.startsWith("cat")) {
+      const path = answer.split(" ")[1];
+      cat(currentDir, path);
+    } else if (answer.startsWith("add")) {
+      const path = answer.split(" ")[1];
+      add(currentDir, path);
+    } else if (answer.startsWith("rn")) {
+      const path = answer.split(" ")[1];
+      const newPath = answer.split(" ")[2];
+      rn(currentDir, path, newPath);
+    } else if (answer.startsWith("mv")) {
+      const path = answer.split(" ")[1];
+      const newPath = answer.split(" ")[2];
+      mv(currentDir, path, newPath);
+    } else if (answer.startsWith("cp")) {
+      const path = answer.split(" ")[1];
+      const newPath = answer.split(" ")[2];
+      cp(currentDir, path, newPath);
+    } else if (answer.startsWith("rm")) {
+      const path = answer.split(" ")[1];
+      rm(currentDir, path);
+    } else if (answer.startsWith("os")) {
+      const arg = answer.split(" ")[1];
+      os(arg);
+    } else if (answer.startsWith("hash")) {
+      const path = answer.split(" ")[1];
+      hash(path);
+    } else if (answer.startsWith("compress")) {
+      const path = answer.split(" ")[1];
+      const newPath = answer.split(" ")[2];
+      compress(path, newPath);
+    } else if (answer.startsWith("decompress")) {
+      const path = answer.split(" ")[1];
+      const newPath = answer.split(" ")[2];
+      decompress(path, newPath);
     } else {
       console.log(`Invalid input`);
     }
